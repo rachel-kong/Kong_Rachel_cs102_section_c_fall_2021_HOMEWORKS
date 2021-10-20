40c1915aadf8504e2e67a5b8dfe3cee5cce03035
rachel-kong <90731091+rachel-kong@users.noreply.github.com>
2021-10-20 17:25:48 -0400

    Update prob-1.c

--- HW2/prob-1/prob-1.c
+++ HW2/prob-1/prob-1.c
@@ -2,19 +2,19 @@
 
 int main()
 {
-    float length, width, perimeter;
+    float length, width, perimeter; /* variables */
 
     
-    printf("What is the length of the rectangle?: ");
+    printf("What is the length of the rectangle?: "); /* user inputs length */
     scanf("%f", &length);
-    printf("What is the width of the rectangle?: ");
+    printf("What is the width of the rectangle?: "); /* user inputs width */
     scanf("%f", &width);
 
     
-    perimeter = 2 * (length + width); /* Formula for the perimeter of a rectangle */
+    perimeter = 2 * (length + width); /* formula for the perimeter of a rectangle */
 
     
-    printf("Perimeter of rectangle = %f units ", perimeter); /* Gives value of the perimeter */
+    printf("Perimeter of rectangle = %f units ", perimeter); /* gives value of the perimeter */
 
     return 0;
 }
